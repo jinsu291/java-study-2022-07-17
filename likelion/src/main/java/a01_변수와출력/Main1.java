c519e5e506066a7e7f23e539dc50e7280b66a49f
jinsu291 <wlstn291@naver.com>
2022-07-17 02:53:58 +0900

    세팅 & a1 1추가

---
+++ likelion/src/main/java/a01_변수와출력/Main1.java
@@ -0,0 +1,16 @@
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+
+class Main1 {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+    }
+}
